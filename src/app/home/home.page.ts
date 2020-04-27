9773d8ec564b05734adc3030b381a2f8a7575c6e
slopesm@gmail.com <slopesm@gmail.com>
2020-04-27 02:56:39 +0200

    changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit{
+    @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
+    map: google.maps.Map;
+    lat = -28.4792625 ;
+    lng = 24.6727135;
+    coordinates = new google.maps.LatLng(this.lat, this.lng);
 
   constructor() {}
+   
+  heatmapData = [ new google.maps.LatLng(-28.4792625, 24.6727135)];
+  SouthAfrica =  new google.maps.LatLng(-28.4792625, 24.6727135)
+  
+
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map,
+  });
+
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 4,
+    mapTypeId: 'satellite'
+  };
+
+ 
+
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, 
+    this.mapOptions);
+    ; 
+   }
+   private heatmap: google.maps.visualization.HeatmapLayer = null;
+  ngAfterViewInit(){
+    
+    this.mapInitializer();
+    this.heatmap = new google.maps.visualization.HeatmapLayer({
+        map: this.map,
+        data: this.heatmapData
+    });
+  }
+
+ 
+
+  
+  
 
 }
+
